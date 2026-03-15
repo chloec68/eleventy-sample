05561101dce7219adecaa062ce9f626a585606cf
Chloé Cilia <chloe.cilia68@gmail.com>
2026-03-15 20:28:47 +0100

    add img plugin

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,9 +1,23 @@
-module.exports = function(eleventyConfig) {
+const { eleventyImageTransformPlugin } = require("@11ty/eleventy-img");
+const fs = require("fs-extra");
+
+module.exports = async function(eleventyConfig) {
+
+  eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
+    formats:["avif","webp","jpeg"],
+    width: ["auto"]
+  });
+
   // Copier les assets (CSS, JS, images)
   eleventyConfig.addPassthroughCopy("src/assets/css");
-    eleventyConfig.addPassthroughCopy("src/assets/icons");
+  eleventyConfig.addPassthroughCopy("src/assets/icons");
   eleventyConfig.addPassthroughCopy("src/assets/scripts");
   eleventyConfig.addPassthroughCopy("src/assets/photographs");
+
+  eleventyConfig.on("eleventy.after", async () => {
+    await fs.copy("_tmp/img/", "_site/img/", { overwrite: true });
+    console.log("📁 Copied optimized images to _site/img/");
+  });
 
   return {
     dir: {
@@ -12,3 +26,6 @@ module.exports = function(eleventyConfig) {
     }
   };
 };
+
+
+
